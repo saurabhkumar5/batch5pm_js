ceb8b3af2510b4736b6fbe646f9b745d75eee96f
saurabh kumar <saurabhkumar9493@gmail.com>
2024-07-16 17:47:07 +0530

    first commit

---
+++ class1.js
@@ -0,0 +1,111 @@
+
+
+// var a = 10
+// var b = 20
+// var b = 30
+
+//   var aman = 10
+        //  var 1a = 10;
+            // var a1 = 10
+            // var _a = 10
+            // var $a = 10
+            
+  
+
+    // console.log($a)
+
+    //  console.log("hello"); 
+    //   console.log("world");
+    //     console.log(123);
+    
+
+    // var a = 10;
+    // var a = 20;
+    //     a = 30;
+    // console.log(a)
+
+
+    // let a = 20;
+    // console.log(a)
+
+    // const a = 10;
+    // console.log(a)
+
+
+
+    // var a = 10;
+    // var a = 20;
+    //     a = 30;
+    // console.log(a)
+
+    // let a = 10;
+    //     a  = 20;
+    // console.log(a)
+
+
+    // const a = 10;
+    // console.log(a)
+
+
+
+    // var a = 10;
+    // let a = 20;
+    // const a = 30;
+    // {
+    //     console.log(a)
+    // }
+
+        //  {} block scope
+
+    // {
+    //     // var a = 10;
+    //     // let a  = 10;
+    //     const a = 20;
+    // }
+    // console.log(a)
+
+
+    // let a = 10;
+
+    // {
+    //      a = 20;
+    // }
+    // console.log(a)
+
+
+    // var a = 10;
+
+    // {
+    //     var a = 20;
+    // }
+    // console.log(a)
+
+
+    // let a = 10;
+    // {
+    //     // const a = 20;
+    //     a = 60;
+    //     console.log(a)
+    // }
+    // console.log(a)
+
+
+
+
+    //    let a  = 10;
+    //     //    a = a+10;
+    //         //   a+=20;
+    //         // a = a+1;
+    //         // a+=1
+    //         // a++; // a = a+1
+    //           a--; a = a-1
+    //        console.log(a)
+
+
+
+    let a = 10;
+            // a++ //post increment
+        //  const b  = ++a;  //pre increment
+
+               const b  =  a++;
+        console.log(b)
