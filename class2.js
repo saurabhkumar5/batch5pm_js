efdcb595510775201ace03932c2936e10f9e7f48
saurabh kumar <saurabhkumar9493@gmail.com>
2024-07-17 20:59:06 +0530

    hello

---
+++ class2.js
@@ -0,0 +1,64 @@
+
+// 1.Number
+// 2. String
+// 3. Boolean
+// 4. undefined
+// 5. null
+// 6. object
+// 7. array
+
+// let a = 10;
+// let b = 20;
+// console.log(typeof(a))
+// console.log(a+b)
+// ""
+// ''
+
+// let a = "aman"; //string
+
+// let a = "10";
+// console.log(typeof(a))
+
+
+    // let a = "true";
+    // console.log(typeof(a))
+
+
+    // 5 < 6
+        // 7 >30
+        //  >=
+        //  <=
+    // console.log(5>6);
+    // console.log(50>=40)
+
+
+    // logical operator
+    //  1. && logical and operator
+    //  2. ||  logical OR operator
+    //  3. ! logical NOT operator
+
+        //   console.log(5>3 && 10>8 && 4>21 && 60>30);
+
+        //    console.log(11>50 || 12>30 || 21>18 || 40>50 || 60>64);
+
+        //    console.log(!(10>50));
+
+      
+        // console.log(10>50 || 32>=320 || 45>=45.1)
+
+        // console.log(45>10 && !(32>56) && 65>10.2)
+
+        // console.log(15>10 && true)
+        // console.log(true && 41<13)
+
+
+
+        let a = 10;  //number
+        // let b = 10;
+        let b = '10'; //string
+        // let c = b;
+        // console.log(a=b);
+
+        // console.log(a==b) //only check value
+
+        console.log(a===b)
