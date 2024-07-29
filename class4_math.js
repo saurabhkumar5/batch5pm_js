a80eeef1896d6091f6806718572f621fb20ace5d
saurabh kumar <saurabhkumar9493@gmail.com>
2024-07-29 17:48:58 +0530

    string

---
+++ class4_math.js
@@ -0,0 +1,16 @@
+
+
+// Math.abs()
+// Math.pow()
+// Math.floor()
+// Math.ceil()
+// Math.random()
+
+// console.log(Math.abs(-5))
+// console.log(Math.pow(3,3));
+// console.log(Math.floor(6.99))
+// console.log(Math.ceil(7.9))
+
+// 0.5 
+
+// console.log(Math.floor(Math.random()*100))
---
+++ class5_string.js
@@ -0,0 +1,103 @@
+
+// let a = "monu ";
+// let b = "sonu"
+   // console.log(typeof(a))
+   // console.log(a.length);
+   // console.log(a[3])
+   // console.log(a+" "+b);
+
+   // console.log(a.concat(b))
+
+
+   // 1. indexOf()
+   // let str = "anuj";
+   // console.log(str.indexOf('a'))
+
+
+   // 2.replace()
+
+   // let a = "anamika";
+   //     console.log(a.replace('anamika','aman'))
+
+   // 3. search()
+
+      //  let a = "hello how are you";
+      // console.log(a.search('ou'))
+
+
+      // 4. toLowerCase()
+
+      // let a = "WELCOME";
+      // console.log(a.toLowerCase());
+
+      // let a = "welcome"
+      // // console.log(a.toUpperCase())
+
+      // if(a.toUpperCase()==='WELCOME'){
+      //    console.log("most welcoem")
+      // }
+
+// 4.includes()
+
+   // let a = "most welcome";
+   // // console.log(a.includes('m'))
+   // if(a.includes('welcomee')){
+   //    console.log("hellooo")
+   // }
+
+   // 5. trim()
+
+   // let a = "             wel   come       ";
+   // console.log(a.trim());
+
+   // 6.startswith()
+
+   // let a = "welcome";
+   // // // console.log(a.startsWith('W'))
+
+   // // if(a.startsWith('wel')){
+   // //    console.log("you can login this website")
+   // // }else{
+   // //    console.log("you cant log this website")
+   // // }
+
+   //  console.log(a.endsWith('come'))
+
+
+   // 7. charAt()
+
+   // let a = "aman";
+   // console.log(a.charAt(3))
+
+
+   // 8.split()
+
+   // let a = "most welcome";
+   // console.log(a.split(' '));
+
+   // 9.repeat
+   // let a = "rohan"
+   // console.log(a.repeat(5))
+
+   // 10. substring()
+
+      // let a = "most welcome"
+      //  console.log(a.substring(2,6))
+
+
+   // 11. slice()
+
+   // let a  = "most welcome";
+   //  console.log(a.slice(-6,-2))
+  
+
+   // let a = "komal";
+   // let b = "aman";
+   // let c = "rohan";
+   
+   // [] //array
+
+   // let friends = ["komal","aman","rohan",123];
+   // console.log(friends)
+
+   // ab start hota h array
