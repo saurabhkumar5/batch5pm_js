7e141903422e48e6f7c6bdfecd78fd6bed624753
saurabh kumar <saurabhkumar9493@gmail.com>
2024-08-07 10:45:40 +0530

    try

--- class10_highOrder.js
+++ class10_highOrder.js
@@ -1,4 +1,4 @@
-let arr = [12,8,6,33,41,32];
+// let arr = [12,8,6,33,41,32];
 
         // for(let i = 0; i<arr.length;i++){
         //     console.log(arr[i])
@@ -6,8 +6,77 @@ let arr = [12,8,6,33,41,32];
 
         // 1. map function
 
-          const data = arr.map((a,b,c)=>{
-                // console.log(a/2)
-                   return a*2
-            })
-            console.log(data)
+          // const data = arr.map((a,b,c)=>{
+          //       // console.log(a/2)
+          //          return a*2
+          //   })
+          //   console.log(data)
+
+      //  const data  = [12,8,6,33,41,32].map((a,b,c)=>{
+      //       // console.log(a/2)
+      //         //  return a*2
+
+      //         if(a%2==0){
+      //           return a
+      //         }
+      //   })
+      //   console.log(data)
+
+
+      // 2. filter
+
+            // let arr  = [12,8,6,33,41,32];
+
+            //       const data = arr.filter((a,b,c)=>{
+            //             // console.log(a,b,c)
+
+            //             if(a%2==0){
+            //               return a
+            //             }
+            //             // return a
+            //         })
+            //         console.log(data)
+
+
+            // 3. sort()
+
+          // let arr  = [12,8,6,33,41,32];
+
+          //         const data = arr.sort((a,b,c)=>{
+          //             // console.log(a,b,c)
+
+          //             return b-a
+          //           })
+          //           console.log(data)
+
+          // 4. forEach function
+
+          // let arr  = [12,8,6,33,41,32];
+               
+          //       const data =  arr.forEach((a,b,c)=>{
+          //           // console.log(a,b,c)
+          //           return a
+          //          })
+          //          console.log(data)
+
+
+          // 5. reduce funtion
+          // let arr  = [12,8,6,33,41,32];
+
+          //     const data =  arr.reduce((a,b,c)=>{
+          //         // console.log(a,b,c)
+          //          return a+b
+          //        },40)
+
+          //        console.log(data)
+
+
+          // setTimeout
+
+          // setTimeout(()=>{
+          //   console.log("hello")
+          // },4000)
+
+          setInterval(()=>{
+            console.log("hello")
+          },3000)
---
+++ class11_tryCatch.js
@@ -0,0 +1,11 @@
+
+let a = 23
+console.log("hello");
+console.log(12);
+try{
+    console.log(a);
+}catch (err){
+       console.log("error",err)
+}
+console.log("kullu");
+console.log(10);
